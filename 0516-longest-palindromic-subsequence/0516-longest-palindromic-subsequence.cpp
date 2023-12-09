9d8382bbf00d80072213713e374371cf8fdfb934
Saurabh Salunke <119050714+SaurabhS55@users.noreply.github.com>
2023-12-09 13:48:59 +0530

    Time: 104 ms (53.11%), Space: 72.6 MB (72.10%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
+       //  string s1=s;
+       //  reverse(s1.begin(),s1.end());
+       //  int n=s.length();
+       //  vector<vector<int>> dp(n+1,vector<int>(n+1));
+       // //base case
+       //  for(int i=0;i<n+1;i++){
+       //      dp[i][0]=0;
+       //  }
+       //  for(int i=0;i<n+1;i++){
+       //      dp[0][i]=0;
+       //  }
+       //  //dp table
+       //  for(int i=1;i<n+1;i++){
+       //      for(int j=1;j<n+1;j++){
+       //          if(s[i-1]==s1[j-1]){
+       //              dp[i][j]=1+dp[i-1][j-1];
+       //          }
+       //          else{
+       //              dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+       //          }
+       //      }
+       //  }
+       //  return dp[n][n];
+        
+        //space optimization
         string s1=s;
         reverse(s1.begin(),s1.end());
         int n=s.length();
-        vector<vector<int>> dp(n+1,vector<int>(n+1));
-       //base case
-        for(int i=0;i<n+1;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<n+1;i++){
-            dp[0][i]=0;
-        }
+        vector<int> dp(n+1,0);
         //dp table
         for(int i=1;i<n+1;i++){
+            vector<int> temp(n+1,0);
             for(int j=1;j<n+1;j++){
                 if(s[i-1]==s1[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    temp[j]=1+dp[j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    temp[j]=max(dp[j],temp[j-1]);
                 }
             }
+            dp=temp;
         }
-        return dp[n][n];
+        return dp[n];
         
     }
 };
